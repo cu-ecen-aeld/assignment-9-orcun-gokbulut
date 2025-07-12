bb362378de70d3d87e5512482402ecff0e4de7d1
Yigit Orcun Gokbulut <orcun.gokbulut@gmail.com>
2025-07-12 17:07:48 +0400

    Assingment 7 Part 2

---
+++ base_external/package/ldd/ldd.mk
@@ -0,0 +1,16 @@
+
+##############################################################
+#
+# AESD-ASSIGNMENTS
+#
+##############################################################
+
+LDD_VERSION = '520f7e8fbd9b892981cad83ec808daf03694f312'
+LDD_SITE = 'git@github.com:cu-ecen-aeld/assignment-7-orcun-gokbulut.git'
+LDD_SITE_METHOD = git
+LDD_GIT_SUBMODULES = YES
+
+LDD_MODULE_SUBDIRS = scull misc-modules
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
